8dd8fb4dd5222df1e48673eb27414d0fc67dad24
Cheenu Khurana <cheenu.ck@gmail.com>
2022-09-18 16:45:47 +0530

    title

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>OnMagellanic</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+
+  )
 }
 
 export default MyApp
