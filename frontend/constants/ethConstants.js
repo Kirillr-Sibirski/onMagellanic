444607d42b966c9eef4137f4b55ed1c619c0e870
Kirillr-Sibirski <93882929+Kirillr-Sibirski@users.noreply.github.com>
2022-09-20 21:12:34 +0200

    Add constant files (for smart contracts)

---
+++ frontend/constants/ethConstants.js
@@ -0,0 +1,4 @@
+export const ethEndpoint = "rinkeby";
+export const COUNContractAddress = "0xB6216A120Ac315e0761718b838C494956096250A";
+export const StakingCOUNContractAddress = "0x0E04F6D17FeaEd6C4A830C11b6DC9c51034C5e2e";
+export const AwardNFTContractAddress = "0xD5B9a81d2ae5fc011aBF77201c65efC29389289a";
