ff0611dbc69391e372d2652c947d4105f3d1d9dc
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2022-09-24 22:35:19 +0200

    Change contract

--- frontend/constants/ethConstants.js
+++ frontend/constants/ethConstants.js
@@ -2,11 +2,11 @@ import awardNFT from './ABI/AwardNFT.json'
 import coun from './ABI/COUN.json'
 import stakingCoun from './ABI/StakingCOUN.json'
 
-export const netwrokChainID = "0x4";
-export const ethEndpoint = "rinkeby";
-export const COUNContractAddress = "0xB6216A120Ac315e0761718b838C494956096250A";
-export const StakingCOUNContractAddress = "0x0E04F6D17FeaEd6C4A830C11b6DC9c51034C5e2e";
-export const AwardNFTContractAddress = "0xD5B9a81d2ae5fc011aBF77201c65efC29389289a";
+export const netwrokChainID = "0x80001"; // Polygon Mumbai testnet
+export const ethEndpoint = "Mumbai";
+export const COUNContractAddress = "0x6C7fc34D009FDb82d95f295e096De2Cb097a7981";
+export const StakingCOUNContractAddress = "0x79c361D8f67bFce91c6D2A9d5492c4A46d0996c3";
+export const AwardNFTContractAddress = "0xeD62cfF87d204A0222905ca53EF155cc02Ff7Fd2";
 export const _awardNFT = awardNFT;
 export const _coun = coun;
 export const _stakingCoun = stakingCoun;
--- frontend/interactions/index.js
+++ frontend/interactions/index.js
@@ -1,21 +1,16 @@
 import { ethers } from "ethers";
 import { COUNContractAddress, StakingCOUNContractAddress, AwardNFTContractAddress, _stakingCoun, awardNFT, coun} from "../constants/ethConstants"; 
 
-export async function stake(provider){ // need to import provider here
+export async function stake(provider){
+    let amount = 10000000000000000000; // 10 COUN in Wei
+    let time = 1209600; // 14 days in Seconds
+    
     // Do all the checks here (if the user has already staked etc.)
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
     let userAddress = await signer.getAddress();
-    const stakingContract = new ethers.Contract(StakingCOUNContractAddress, _stakingCoun, signer);
-  
-    const req = await stakingContract.totalSupply()
-    console.log(req, userAddress)
 
-    // All of these are used for payable smart contract
-    /*const tx = await stakingContract.setRewardsDuration({ gasPrice: 20e9 }); 
-    console.log(`Transaction hash: ${tx.hash}`);
-  
-    const receipt = await tx.wait();
-    console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
-    console.log(`Gas used: ${receipt.gasUsed.toString()}`);*/
+    await COUNContractAddress.approve(StakingCOUNContractAddress, amount); // Approve for transferFrom function in StakingContract
+    const stakingContract = new ethers.Contract(StakingCOUNContractAddress, _stakingCoun, signer);
+    stakingContract.stake(amount);
   }
