6eb07e6983b0b3003bcfae5d4f8df4035b268ff4
Cheenu Khurana <cheenu.ck@gmail.com>
2022-09-26 00:48:20 +0530

    little design changes

--- frontend/components/home.js
+++ frontend/components/home.js
@@ -2,28 +2,28 @@ export default function HomeComponent() {
     return (
         <div>
             <div className="max-w-xl">
-                <h2 className="">A Better Way to Live</h2>
-                <p>Increase concentration during work time or relax more effectively. Our challenges help to improve quality of life along with getting extra rewards.</p>
+                <h1 className="text-3xl font-ld">A Better Way to Live</h1>
+                <p className="mt-4 text-lg">Increase concentration during work time or relax more effectively. Our challenges help to improve quality of life along with getting extra rewards.</p>
                 <p>Start building the habits you deserve.</p>
-                <button className="px-6 py-4">Start Now</button>
+                <button className="mt-4 px-10 py-3 bg-orange-600 text-white">Start Now</button>
             </div>
 
             <div className="mt-6">
                 <div className="text-center max-w-xl m-auto">
-                    <h2 className="">Have fun while achieving</h2>
-                    <p>Our gameplay will give you the utimate push to achieve. Compete with others and earn higher rewards.</p>
+                    <h2 className="text-2xl font-bold">Have fun while achieving</h2>
+                    <p className="mt-2 text-lg">Our gameplay will give you the utimate push to achieve. Compete with others and earn higher rewards.</p>
                 </div>
-                <div className="flex justify-between mt-4">
+                <div className="mt-4 flex justify-between">
                     <div>
-                        <h3 className="text-center">Join a Challenge</h3>
+                        <h3 className="text-xl font-bold text-center">Join a Challenge</h3>
                         <p>Option to stake token (Dai, eth) when joining the challenge.</p>
                     </div>
                     <div>
-                        <h3 className="text-center">Complete tasks</h3>
+                        <h3 className="text-xl font-bold text-center">Complete tasks</h3>
                         <p>Complete task regulary to stay in the challenge. Feel proud as you hit your life goals.</p>
                     </div>
                     <div>
-                        <h3 className="text-center">Watch rewards accrue</h3>
+                        <h3 className="text-xl font-bold text-center">Watch rewards accrue</h3>
                         <p>As players leave the game, their tokens are distributed to the remaining players.</p>
                     </div>
                 </div>
@@ -31,7 +31,7 @@ export default function HomeComponent() {
 
             <div className="mt-8">
                 <div className="text-center max-w-xl m-auto">
-                <h2>Hit your goals and win</h2>
+                <h2 className="text-2xl font-bold">Hit your goals and win</h2>
                 <p>Players who complete a challenge get back their staked tokens. Players who leave the challenge unfinished do not get back their tokens. Those are distributed equally among the players who completed the challenge. Players who complete the challenge are also minted an nft.</p>
             </div>
             </div>
